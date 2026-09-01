c0ccc122c3db1fc779881445853161c2565c88f6
lidaixingchen <3506962253@qq.com>
2026-09-01 23:49:35 +0800

    refactor(grid): collapse shallow wrappers into GridCoordinator

--- src/features/grid/index.ts
+++ src/features/grid/index.ts
@@ -1,34 +1,4 @@
-import { GridCoordinator } from "./coordinator";
-
 export * from "./constants";
 export * from "./calculator";
 export * from "./scoped-observer";
 export * from "./coordinator";
-export * from "./adapter";
-
-export const FourColumnGrid = {
-  css: GridCoordinator.GRID_CSS,
-
-  getItemsPerRow(): number {
-    return GridCoordinator.getInstance().getItemsPerRow();
-  },
-
-  balanceGrid(): void {
-    GridCoordinator.getInstance().rebalance();
-  },
-
-  startObserver(): void {
-    GridCoordinator.getInstance().init();
-  },
-
-  run(): void {
-    if (!/youtube\.com/.test(window.location.host)) {
-      return;
-    }
-    GridCoordinator.getInstance().init();
-  },
-
-  destroy(): void {
-    GridCoordinator.getInstance().destroy();
-  }
-};
